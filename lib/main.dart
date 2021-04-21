b93de53beac889bd2d9c9b90d8caa802e0dfb066
chathurikaPerera <pererachathurika96@gmail.com>
2021-04-21 14:24:53 +0530

    welcome page update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:care_me/screens/background.dart';
 import 'package:care_me/screens/welcome.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -20,46 +21,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-// class welcome extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     Size size = MediaQuery
-//         .of(context)
-//         .size;
-//     return Scaffold(
-//       body: Container(
-//         height: 200.0,
-//         width: 200.0,
-//         decoration: BoxDecoration(
-//           image: DecorationImage(
-//             image: AssetImage(
-//                 'assets/images/image2.png'
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//
-//
-//     /*return Container(
-//       height: size.height,
-//       width: double.infinity,
-//       child: Stack(
-//         alignment: Alignment.center,
-//         children: <Widget>[
-//         Positioned(
-//         top: 0,
-//         left: 0,
-//         child: Image.asset(
-//           "assets/images/image2.png",
-//           width: size.width * 0.3,
-//         ),
-//         ),
-//         ],
-//       ),
-//     );
-//     */
-//   }
-// }
-//
-//
--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,22 +1,24 @@
+import 'package:care_me/screens/background.dart';
 import 'package:flutter/material.dart';
 
 class welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          height: 200.0,
-          width: 200.0,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(
-                  'assets/images/image2.png'
-              ),
-            ),
-          ),
-        ),
-      ),
+    return Background(
+
+        // child: Column(
+        //   Container(
+        //     height: 200.0,
+        //     width: 200.0,
+        //     decoration: BoxDecoration(
+        //       image: DecorationImage(
+        //         image: AssetImage(
+        //             'assets/images/image2.png'
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        // )
     );
   }
 }
